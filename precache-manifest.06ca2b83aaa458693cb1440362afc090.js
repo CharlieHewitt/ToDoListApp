ab895861082d602c427a6c19eaedbdc25d003c60
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-05-02 23:07:22 +0100

    Updates

---
+++ precache-manifest.06ca2b83aaa458693cb1440362afc090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976fb8c62906baef0b1003760321cb49",
+    "url": "/ToDoListApp/index.html"
+  },
+  {
+    "revision": "d8a3f656bb24b946feb7",
+    "url": "/ToDoListApp/static/css/main.ab580cc4.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/ToDoListApp/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "d8a3f656bb24b946feb7",
+    "url": "/ToDoListApp/static/js/main.346db3f5.chunk.js"
+  },
+  {
+    "revision": "7f5147409da8c65523c7",
+    "url": "/ToDoListApp/static/js/runtime~main.82f07f95.js"
+  }
+]);
