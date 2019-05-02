fdc08b647685ad92ea4d40e0f734c15f176149ea
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-05-02 22:07:45 +0100

    add todo item

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
 
 class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.addItem = this.addItem.bind(this);
+
+        this.state = {
+            items: []
+        };
+
+    }
+
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value = "";
+        }
+
+        console.log(itemArray);
+
+        e.preventDefault();
+
+    }
+
     render() {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="enter a task"></input>
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a}
+                            placeholder="enter a task"></input>
                         <button type="submit">add</button>
                     </form>
                 </div>
